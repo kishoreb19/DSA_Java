6f4fd92c4b8c01c76ed080c9d2aba3c2ea9ac575
kishoreb19 <kishorebhosley@gmail.com>
2024-04-21 12:22:24 +0530

    New Approach

--- binarySearchTree/ValidateBST.java
+++ binarySearchTree/ValidateBST.java
@@ -11,20 +11,34 @@ public class ValidateBST {
         }
     }
     //------------------------------------------------------------------------------------------------------------------
+//    static boolean solve(BinaryTreeNode root, int min, int max){
+//        if(root==null){
+//            return true;
+//        }
+//        if(min<=root.data && root.data<=max){
+//            boolean left = solve(root.left, min, root.data);
+//            boolean right = solve(root.right,root.data,max);
+//            return left && right;
+//        }else{
+//            return false;
+//        }
+        //Time Complexity - O(N)
+        //Space Complexity - O(H)
+        //https://www.naukri.com/code360/problems/validate-bst_799483?leftPanelTab=0&leftPanelTabValue=PROBLEM
+
+    //This code is failing some testcases !
+//    }
     static boolean solve(BinaryTreeNode root, int min, int max){
         if(root==null){
             return true;
         }
-        if(min<=root.data && root.data<=max){
-            boolean left = solve(root.left, min, root.data);
-            boolean right = solve(root.right,root.data,max);
-            return left && right;
-        }else{
+        if(root.data>=max || root.data<=min){
             return false;
         }
+        return solve(root.left,min,root.data) && solve(root.right,root.data,max);
         //Time Complexity - O(N)
         //Space Complexity - O(H)
-        //https://www.naukri.com/code360/problems/validate-bst_799483?leftPanelTab=0&leftPanelTabValue=PROBLEM
+        //https://leetcode.com/problems/validate-binary-search-tree/description/
     }
     public static boolean validateBST(BinaryTreeNode<Integer> root) {
         // Write your code here
